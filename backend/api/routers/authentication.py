c1ede3875550b92bb46ea2f3b0b15305f0a2fab9
Christy <csc263@cornell.edu>
2025-12-07 14:23:45 -0500

    Modified login error handling and messages

---
+++ backend/api/routers/authentication.py
@@ -0,0 +1,37 @@
+"""
+Use APIRouter for user registration and login endpoints
+"""
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from ..schemas import *
+from ..auth import *
+from ..storage import *
+
+router = APIRouter(prefix="/auth", tags=["authentication"])
+
+# Registration endpoint
+@router.post("/register", response_model=User)
+async def register(user: User):
+    return create_user(user)
+
+
+# Login endpoint
+# Called when signing in with username and password and return access token (to be used during active duration)
+@router.post("/login", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # the data that we're going to accept to generate a jwt token is going to be a username and password. 
+    # depends on OAuth2PasswordRequestForm to parse the data
+    user = authenticate_user(form_data.username, form_data.password)
+    if user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="User not registered", headers={"WWW-Authenticate": "Bearer"})
+    elif user == False:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                            detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    # automatically converted to Token object
+    return {"access_token": access_token, "token_type": "bearer"}
+
---
+++ backend/api/routers/users.py
@@ -0,0 +1,34 @@
+"""
+Use APIRouter for protected user endpoints
+"""
+
+from typing import List
+from fastapi import APIRouter, Depends, File, UploadFile, status
+from ..schemas import *
+from ..auth import *
+from ..jobs import create_job, list_jobs
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.get("/me/", response_model=User)
+async def read_users_me(current_user: UserInDB = Depends(get_current_active_user)):
+    # relies on get_current_active_user which relies on get_current_user which relies 
+    # on oauth_2_scheme which relies on token. So, need to first access the token by *logging in*
+    # Convert UserInDB to User (exclude hashed_password from response)
+    return User(
+        username=current_user.username,
+        password=current_user.password,
+        disabled=current_user.disabled
+    )
+
+@router.get("/me/jobs/", response_model=List[Job])
+async def read_jobs(current_user: UserInDB = Depends(get_current_active_user)):
+    return list_jobs(current_user.username)
+
+
+@router.post("/me/jobs/", response_model=Job, status_code=status.HTTP_201_CREATED)
+async def upload_job(
+    file: UploadFile = File(...),
+    current_user: UserInDB = Depends(get_current_active_user),
+    ):
+    return await create_job(current_user.username, file)
