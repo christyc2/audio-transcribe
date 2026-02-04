fe07cb973d70e08eaf56b9009ee463779cf4bbad
Christy <csc263@cornell.edu>
2026-02-04 00:28:11 -0500

    Add config for vercel

--- backend/api/main.py
+++ backend/api/main.py
@@ -21,6 +21,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "audio-transcribe-five.vercel.app",
+    "audio-transcribe-christyc2s-projects.vercel.app"
 ]
 
 app.add_middleware(
